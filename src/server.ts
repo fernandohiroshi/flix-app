43592cac54b96be73ca21bb6127776e3d7c942f5
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2025-04-25 05:29:21 -0300

    chore: setup Sequelize

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
-import express from "express";
+import express from 'express'
+import { sequelize } from './database'
 
-const app = express();
+const app = express()
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`Server started successfully at port ${PORT}`);
-});
+  sequelize.authenticate().then(() => {
+    console.log('DB connection successful')
+  })
+  console.log(`Server started successfully at port ${PORT}`)
+})
