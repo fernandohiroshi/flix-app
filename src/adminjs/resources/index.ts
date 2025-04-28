3fdec92d397b0dc93fbf0ef0e9ed36bffa4c2a79
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2025-04-28 14:14:58 -0300

    feat: add relationships between models

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,10 +1,15 @@
 import { ResourceWithOptions } from 'adminjs'
-import { Category } from '../../models'
+import { Category, Course } from '../../models'
 import { CategoryResourceOptions } from './category'
+import { courseResourceOptions } from './course'
 
 export const adminJsResources: ResourceWithOptions[] = [
   {
     resource: Category,
     options: CategoryResourceOptions,
+  },
+  {
+    resource: Course,
+    options: courseResourceOptions,
   },
 ]
