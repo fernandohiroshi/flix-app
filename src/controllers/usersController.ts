5d6bd755d44acd356a913c2ec7cf31d1864d87ae
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2025-06-02 04:39:25 -0300

    feat: updating password

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,6 +1,7 @@
 import { Response } from 'express'
 import { AuthenticatedRequest } from '../middlewares/auth'
 import { userService } from '../services/userService'
+import { error } from 'console'
 
 export const usersController = {
   // GET /users/current
@@ -30,6 +31,35 @@ export const usersController = {
       })
 
       return res.json(updatedUser)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
+  // PUT /users/current/password
+  updatePassword: async (req: AuthenticatedRequest, res: Response) => {
+    const user = req.user
+    const { currentPassword, newPassword } = req.body
+
+    if (!user) {
+      return res.status(401).json({ message: 'unauthorized' })
+    }
+
+    try {
+      user.checkPassword(currentPassword, async (err, isSame) => {
+        if (err) {
+          return res.status(400).json({ message: err.message })
+        }
+
+        if (!isSame) {
+          return res.status(400).json({ message: 'incorrect password' })
+        }
+
+        await userService.updatePassword(user.id, newPassword)
+        return res.status(204).send()
+      })
     } catch (err) {
       if (err instanceof Error) {
         return res.status(400).json({ message: err.message })
